64f2b911eb0b99c2bd1b20734f3678894f73d224
Adrian Komuda <=>
2022-02-16 11:12:22 +0100

    Object in memory wasn't cleared before starting a new copy.

--- src/main/java/pl/adrian_komuda/manipulate_volume_object/commands/player/particular_commands/StopCopying.java
+++ src/main/java/pl/adrian_komuda/manipulate_volume_object/commands/player/particular_commands/StopCopying.java
@@ -2,6 +2,8 @@ package pl.adrian_komuda.manipulate_volume_object.commands.player.particular_com
 
 import org.bukkit.entity.Player;
 import pl.adrian_komuda.manipulate_volume_object.commands.PlayerCommandsPerformers;
+import pl.adrian_komuda.manipulate_volume_object.messages.ErrorMessages;
+import pl.adrian_komuda.manipulate_volume_object.services.operations.CopyService;
 
 import java.util.List;
 
@@ -9,5 +11,12 @@ public class StopCopying implements PlayerCommandsPerformers {
     @Override
     public void perform(Player player, String command, List<String> args) {
         player.sendMessage("Performing " + this.getClass().getSimpleName()); // all logic
+//
+//        try {
+//            CopyService copyService = new CopyService(player);
+//            copyService.abortProcess();
+//        } catch (IllegalArgumentException e) {
+//            player.sendMessage(ErrorMessages.NOTHING_TO_ABORT.getMessage());
+//        }
     }
 }
